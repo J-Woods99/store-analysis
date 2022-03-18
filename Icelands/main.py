73a0b84ef21be9ebf635e3945783eb3fc0d0d583
J-Hayward <jh01469@surrey.ac.uk>
2022-03-18 16:15:48 +0000

    initial

---
+++ Icelands/main.py
@@ -0,0 +1,30 @@
+###########################
+#                         #
+#    SHOP FOOD SCALPER    #
+#      BY MR. WOODS       #
+###########################
+
+# NOTES
+#https://www.iceland.co.uk/sitemap
+
+# MODULES
+# functions to import
+# BeautifulSoup4
+# Pandas
+
+# CONSTANTS
+
+# VARIABLES
+
+# SCALPER INIT
+
+# EXCEL INIT
+
+# SCALPER FUNCTION
+
+# SEND TO EXCEL
+
+# MAIN
+
+
+
